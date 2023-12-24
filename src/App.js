6330bcf029d4a5ad639a7613a17101b2df31b076
Chiranjibi Karki <ckarki70@gmail.com>
2023-12-23 21:11:40 -0500

    router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<>Home page!</>} />
+        <Route path="/artistdetails" element={<>Artist details</>} />
+      </Routes>
+    </Router>
   );
 }
 
