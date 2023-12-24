13b6720445f50b8bdb25e1d851099980c0a6031c
Chiranjibi Karki <ckarki70@gmail.com>
2023-12-24 00:43:26 -0500

    song list component ui complete

--- src/components/ArtistDetails.jsx
+++ src/components/ArtistDetails.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import SongList from './SongList';
 
 function ArtistDetails() {
   return (
-    <div className='h-95vh bg-neutral-200 w-2/3 rounded-md flex flex-col gap-3 pb-4'>
+    <div className='h-95vh bg-neutral-200 w-2/3 rounded-md flex flex-col relative p-4'>
+        <SongList/>
     </div>
   )
 }
--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import { RiShareBoxLine } from "react-icons/ri";
+import { CiMenuBurger } from "react-icons/ci";
+import { Dropdown } from "antd";
+
+const items = [
+  {
+    key: "1",
+    label: (
+      <p
+      >
+        1st menu item 1st menu item 1st menu item 1st menu item
+      </p>
+    ),
+  },
+  {
+    key: "2",
+    label: (
+      <p
+      >
+        1st menu item 1st menu item 1st menu item 1st menu item
+      </p>
+    ),
+  },
+  {
+    key: "3",
+    label: (
+      <p
+      >
+        1st menu item 1st menu item 1st menu item 1st menu item
+      </p>
+    )
+  }
+];
 
 function SongList() {
   return (
-    <div>SongList</div>
-  )
+    <div className="bg-white rounded-lg w-full px-2 py-1 flex items-center">
+      <div className="grow flex items-center gap-3">
+        <Dropdown
+          placement="bottomLeft"
+          menu={{
+            items,
+            style: { marginTop: "5px" },
+            onClick:(l)=>console.log(l)
+          }}
+          trigger={["click"]}
+        >
+          <CiMenuBurger size={21} className="hover:cursor-pointer" />
+        </Dropdown>
+        <p>Song title</p>
+      </div>
+
+      <div className="self-end">
+        <RiShareBoxLine size={23} className="hover:cursor-pointer" />
+      </div>
+    </div>
+  );
 }
 
-export default SongList
+export default SongList;
