d7ef1982e2db89050028df91cff3ddd3230bf6c9
Chiranjibi Karki <ckarki70@gmail.com>
2023-12-24 01:35:10 -0500

    song details component ui complete

--- src/components/SongDetails.jsx
+++ src/components/SongDetails.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 function SongDetails() {
   return (
-    <div>SongDetails</div>
+    <div className="w-full bg-white rounded-md bottom-3 left-0">
+      <div className='m-1 bg-neutral-200 text-black rounded-lg w-2/5 px-2 font-bold text-sm'>
+        <p>Song title 1 Details</p>
+      </div>
+      <div className='flex flex-col items-start py-2 px-4 gap-2'>
+      <div>
+        <span>Composer: </span>
+        <span>Hi</span>
+      </div>
+      <div>
+        <span>Producer: </span>
+        <span>Hi</span>
+      </div>
+      <div>
+        <span>Production Date: </span>
+        <span>Hi</span>
+      </div>
+      <div>
+        <span className='text-base'>Awards: </span>
+        <span>Hi</span>
+      </div>
+    </div>
+  </div>
   )
 }
 
--- src/components/SongLyrics.jsx
+++ src/components/SongLyrics.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 function SongLyrics() {
   return (
-    <div>SongLyrics</div>
+    <div className='w-full bg-white rounded-md grow overflow-y-auto scroll-smooth scrollbar'>
+      <div className='m-1 bg-neutral-200 text-black rounded-lg w-2/5 px-2 font-bold text-sm'>
+        <p>Song title 1 Lyrics</p>
+      </div>
+       <p className='px-2'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam placeat, nesciunt eos incidunt voluptatibus ad dicta consectetur deserunt sed fugit natus, provident, suscipit ratione veniam perspiciatis! Debitis, nisi minima. 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam placeat, nesciunt eos incidunt voluptatibus ad dicta consectetur deserunt sed fugit natus, provident, suscipit ratione veniam perspiciatis! Debitis, nisi minima.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam placeat, nesciunt eos incidunt voluptatibus ad dicta consectetur deserunt sed fugit natus, provident, suscipit ratione veniam perspiciatis! Debitis, nisi minima.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam placeat, nesciunt eos incidunt voluptatibus ad dicta consectetur deserunt sed fugit natus, provident, suscipit ratione veniam perspiciatis! Debitis, nisi minima. 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam placeat, nesciunt eos incidunt voluptatibus ad dicta consectetur deserunt sed fugit natus, provident, suscipit ratione veniam perspiciatis! Debitis, nisi minima.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam placeat, nesciunt eos incidunt voluptatibus ad dicta consectetur deserunt sed fugit natus, provident, suscipit ratione veniam perspiciatis! Debitis, nisi minima.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam placeat, nesciunt eos incidunt voluptatibus ad dicta consectetur deserunt sed fugit natus, provident, suscipit ratione veniam perspiciatis! Debitis, nisi minima. 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam placeat, nesciunt eos incidunt voluptatibus ad dicta consectetur deserunt sed fugit natus, provident, suscipit ratione veniam perspiciatis! Debitis, nisi minima.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam placeat, nesciunt eos incidunt voluptatibus ad dicta consectetur deserunt sed fugit natus, provident, suscipit ratione veniam perspiciatis! Debitis, nisi minima.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam placeat, nesciunt eos incidunt voluptatibus ad dicta consectetur deserunt sed fugit natus, provident, suscipit ratione veniam perspiciatis! Debitis, nisi minima. 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam placeat, nesciunt eos incidunt voluptatibus ad dicta consectetur deserunt sed fugit natus, provident, suscipit ratione veniam perspiciatis! Debitis, nisi minima.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam placeat, nesciunt eos incidunt voluptatibus ad dicta consectetur deserunt sed fugit natus, provident, suscipit ratione veniam perspiciatis! Debitis, nisi minima.</p>
+    </div>
   )
 }
 
